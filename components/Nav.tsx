6f7b8da80070acb9fcb14e7f15ad283ce2f9d36d
soham2k06 <sohmm.dev@gmail.com>
2024-01-20 16:11:12 +0530

    Added bg color

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,12 @@
+import { Accessibility, AirVent } from "lucide-react";
 import DarkModeToggle from "./DarkModeToggle";
 
 function Nav() {
   return (
-    <div className="flex items-center justify-between max-w-7xl mx-auto p-4">
-      <h2>Scorer</h2>
+    <div className="flex items-center justify-between max-w-7xl mx-auto p-4 bg-primary-foreground">
+      <h2 className="text-3xl flex items-center gap-2 font-semibold tracking-tight">
+        <AirVent /> Scorer
+      </h2>
       <DarkModeToggle />
     </div>
   );
