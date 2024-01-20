ff0eca3b78c740361683f5a09cdb2f2e11ed92a3
soham2k06 <sohmm.dev@gmail.com>
2024-01-20 16:11:30 +0530

    removed title

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import ScorerLayout from "@/components/scorer/ScorerLayout";
 export default function Home() {
   return (
     <main className="flex justify-center items-center flex-col">
-      <h1>Scorer</h1>
       <ScorerLayout />
     </main>
   );
