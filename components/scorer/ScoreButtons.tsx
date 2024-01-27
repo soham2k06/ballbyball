41d2278cc202dda958cc017913d41e794904ad0b
soham2k06 <sohmm.dev@gmail.com>
2024-01-27 17:33:13 +0530

    styled popover

--- components/scorer/ScoreButtons.tsx
+++ components/scorer/ScoreButtons.tsx
@@ -1,6 +1,6 @@
 import { Button } from "../ui/button";
 import { CardContent } from "../ui/card";
-import { MouseEventHandler } from "react";
+import { MouseEventHandler, useState } from "react";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 function ScoreButtons({
@@ -10,6 +10,7 @@ function ScoreButtons({
   handleScore: MouseEventHandler<HTMLButtonElement>;
   ballEvents: Record<string, string>;
 }) {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   return (
     <CardContent className="space-y-2 p-0">
       <div className="flex gap-1 justify-center w-full">
@@ -52,7 +53,11 @@ function ScoreButtons({
         >
           Wd
         </Button>
-        <Popover>
+        <Popover
+          open={isPopoverOpen}
+          modal
+          onOpenChange={() => setIsPopoverOpen((prev) => !prev)}
+        >
           <PopoverTrigger asChild>
             <Button
               variant="secondary"
@@ -62,14 +67,31 @@ function ScoreButtons({
               NB
             </Button>
           </PopoverTrigger>
-          <PopoverContent>
-            {Object.keys(ballEvents)
-              .filter((ball) => ball !== "-1" && ball !== "-2")
-              .map((event) => (
-                <Button value={`-3${event}`} onClick={handleScore}>
-                  {event}
-                </Button>
-              ))}
+          <PopoverContent className="w-60">
+            <div className="w-full grid grid-cols-3 gap-2">
+              {Object.keys(ballEvents)
+                .filter(
+                  (ball) => ball !== "-1" && ball !== "-2" && ball !== "-3"
+                )
+                .map((event) => (
+                  <Button
+                    className={
+                      event === "6"
+                        ? "bg-amber-400 font-semibold text-amber-950 dark:bg-amber-600 dark:text-amber-50"
+                        : event === "4"
+                        ? "bg-emerald-500 text-emerald-50 dark:bg-emerald-600"
+                        : "bg-secondary text-secondary-foreground"
+                    }
+                    value={`-3${event}`}
+                    onClick={(e) => {
+                      handleScore(e);
+                      setIsPopoverOpen(false);
+                    }}
+                  >
+                    {event}
+                  </Button>
+                ))}
+            </div>
           </PopoverContent>
         </Popover>
 
