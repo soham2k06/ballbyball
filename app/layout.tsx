938c1eabf957d46bf075b71e59996b43666652c9
soham2k06 <sohmm.dev@gmail.com>
2024-01-27 22:59:55 +0530

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,6 +80,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎾</text></svg>"
+        />
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
