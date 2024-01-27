2e0e27b2e4e668b1fda372da261ac7374925d526
soham2k06 <sohmm.dev@gmail.com>
2024-01-27 21:41:29 +0530

    showing empty texts on drawer when no data

--- components/scorer/FullOverSummary.tsx
+++ components/scorer/FullOverSummary.tsx
@@ -20,30 +20,37 @@ function FullOverSummary({ overSummaries }: { overSummaries: EventType[][] }) {
         <DrawerHeader className="pt-6 pb-4 mb-2 shadow shadow-muted">
           <DrawerTitle className="text-2xl">Overs Summary</DrawerTitle>
         </DrawerHeader>
-        <ul className="divide-y p-2 max-h-[calc(100dvh-120px)] min-h-96 overflow-y-auto">
-          {overSummaries.map((over, overI) => {
-            const runs = calcRuns(over);
-            const wickets = over.filter((ball) => ball === "-1").length;
-            return (
-              <div
-                key={overI}
-                className="flex gap-4 py-4 items-center first:pt-0"
-              >
-                <div className="font-bold whitespace-nowrap min-w-28 flex gap-2">
-                  <h3>Over {overI + 1}</h3>
-                  <span>
-                    ({runs}/{wickets})
-                  </span>
+        {overSummaries.length > 0 ? (
+          <ul className="divide-y p-2 max-h-[calc(100dvh-120px)] min-h-96 overflow-y-auto">
+            {overSummaries.map((over, overI) => {
+              const runs = calcRuns(over);
+              const wickets = over.filter((ball) => ball === "-1").length;
+              return (
+                <div
+                  key={overI}
+                  className="flex gap-4 py-4 items-center first:pt-0"
+                >
+                  <div className="font-bold whitespace-nowrap min-w-28 flex gap-2">
+                    <h3>Over {overI + 1}</h3>
+                    <span>
+                      ({runs}/{wickets})
+                    </span>
+                  </div>
+                  <ul className="flex gap-2 overflow-x-auto">
+                    {over.map((ball, ballI) => (
+                      <BallSummary key={ballI} event={ball} size="sm" />
+                    ))}
+                  </ul>
                 </div>
-                <ul className="flex gap-2 overflow-x-auto">
-                  {over.map((ball, ballI) => (
-                    <BallSummary key={ballI} event={ball} size="sm" />
-                  ))}
-                </ul>
-              </div>
-            );
-          })}
-        </ul>
+              );
+            })}
+          </ul>
+        ) : (
+          <div className="h-full flex min-h-96 flex-col items-center justify-center text-center space-y-2">
+            <h2 className="text-2xl font-bold">There is no data to show!</h2>
+            <p>Start adding runs to see data</p>
+          </div>
+        )}
       </DrawerContent>
     </Drawer>
   );
--- components/scorer/OverStats.tsx
+++ components/scorer/OverStats.tsx
@@ -14,7 +14,6 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "../ui/drawer";
-import { EventType } from "@/types";
 
 function OverStats({
   chartSummaryData,
@@ -24,31 +23,6 @@ function OverStats({
   const renderCustomizedLabel = (props: any) => {
     const { x, y, width, value } = props;
     const radius = 10;
-
-    // if (!value) return null;
-    // return (
-    //   <>
-    //     {Array.from({ length: value }).map((_, i) => (
-    //       <g key={i}>
-    //         <circle
-    //           cx={x + width / 2}
-    //           cy={y - radius * (2 * i + 1)}
-    //           r={radius}
-    //           style={{ fill: "hsl(var(--destructive))" }}
-    //         />
-    //         <text
-    //           x={x + width / 2}
-    //           y={y - radius * (2 * i + 1)}
-    //           fill="#fff"
-    //           textAnchor="middle"
-    //           dominantBaseline="middle"
-    //         >
-    //           w
-    //         </text>
-    //       </g>
-    //     ))}
-    //   </>
-    // );
 
     return (
       <text
@@ -68,42 +42,34 @@ function OverStats({
       <DrawerTrigger asChild>
         <Button className="w-full">Stats</Button>
       </DrawerTrigger>
-      <DrawerContent className="p-2">
+      <DrawerContent>
         <DrawerHeader className="pt-6 pb-4 mb-2 shadow shadow-muted">
           <DrawerTitle className="text-2xl">Run rate bar</DrawerTitle>
         </DrawerHeader>
-        <div className="h-96">
-          <div className="h-full max-w-7xl mx-auto">
-            <ResponsiveContainer width="100%" height="100%">
-              <BarChart
-                width={100}
-                height={40}
-                data={chartSummaryData}
-                // data={[
-                //   { i: 1, runs: 12, wickets: 2 },
-                //   { i: 2, runs: 5, wickets: 1 },
-                //   { i: 3, runs: 6 },
-                //   { i: 4, runs: 18 },
-                //   { i: 5, runs: 4 },
-                //   { i: 6, runs: 3 },
-                //   { i: 7, runs: 0 },
-                //   { i: 8, runs: 0 },
-                //   { i: 9, runs: 0 },
-                // ]}
-              >
-                <XAxis dataKey="name" />
-                <Bar
-                  dataKey="runs"
-                  style={{
-                    fill: "hsl(var(--foreground))",
-                    opacity: 0.9,
-                  }}
-                >
-                  <LabelList dataKey="runs" content={renderCustomizedLabel} />
-                </Bar>
-              </BarChart>
-            </ResponsiveContainer>
-          </div>
+        <div className="h-96 p-2">
+          {chartSummaryData.length > 0 ? (
+            <div className="h-full max-w-7xl mx-auto">
+              <ResponsiveContainer width="100%" height="100%">
+                <BarChart width={100} height={40} data={chartSummaryData}>
+                  <XAxis dataKey="name" />
+                  <Bar
+                    dataKey="runs"
+                    style={{
+                      fill: "hsl(var(--foreground))",
+                      opacity: 0.9,
+                    }}
+                  >
+                    <LabelList dataKey="runs" content={renderCustomizedLabel} />
+                  </Bar>
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
+          ) : (
+            <div className="h-full flex flex-col items-center justify-center text-center space-y-2">
+              <h2 className="text-2xl font-bold">There is no data to show!</h2>
+              <p>Start adding runs to see data</p>
+            </div>
+          )}
         </div>
       </DrawerContent>
     </Drawer>
