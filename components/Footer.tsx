7d201d346f275811359ceb292dc259734369ca1c
soham2k06 <sohmm.dev@gmail.com>
2024-01-27 21:23:48 +0530

    a minor footer responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import DarkModeToggle from "./DarkModeToggle";
 
 function Footer() {
   return (
-    <div className="text-muted-foreground border-t mt-4 text-sm p-2 fixed w-full bottom-0">
+    <div className="text-muted-foreground border-t mt-4 text-sm p-2 md:fixed w-full md:bottom-0">
       <div className="max-w-7xl mx-auto flex justify-between w-full items-center">
         <p>
           Created by{" "}
