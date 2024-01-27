bca9c078569a189a7641c0857ea18c2b0419bbbd
soham2k06 <sohmm.dev@gmail.com>
2024-01-27 19:50:38 +0530

    shifted no ball popover into component

---
+++ components/scorer/NoballPopver.tsx
@@ -0,0 +1,57 @@
+import { MouseEventHandler, useState } from "react";
+import { Button } from "../ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+function NoballPopver({
+  handleScore,
+  ballEvents,
+}: {
+  handleScore: MouseEventHandler<HTMLButtonElement>;
+  ballEvents: Record<string, string>;
+}) {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  return (
+    <Popover
+      open={isPopoverOpen}
+      modal
+      onOpenChange={() => setIsPopoverOpen((prev) => !prev)}
+    >
+      <PopoverTrigger asChild>
+        <Button
+          size="lg"
+          variant="secondary"
+          className="w-full h-16 text-lg text-muted-foreground"
+          value="-3"
+        >
+          NB
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-60">
+        <div className="w-full grid grid-cols-3 gap-2">
+          {Object.keys(ballEvents)
+            .filter((ball) => ball !== "-1" && ball !== "-2" && ball !== "-3")
+            .map((event, i) => (
+              <Button
+                key={i}
+                className={
+                  event === "6"
+                    ? "bg-amber-400 font-semibold text-amber-950 dark:bg-amber-600 dark:text-amber-50"
+                    : event === "4"
+                    ? "bg-emerald-500 text-emerald-50 dark:bg-emerald-600"
+                    : "bg-secondary text-secondary-foreground"
+                }
+                value={`-3${event}`}
+                onClick={(e) => {
+                  handleScore(e);
+                  setIsPopoverOpen(false);
+                }}
+              >
+                {event}
+              </Button>
+            ))}
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+export default NoballPopver;
--- components/scorer/ScoreButtons.tsx
+++ components/scorer/ScoreButtons.tsx
@@ -1,7 +1,8 @@
 import { Button } from "../ui/button";
 import { CardContent } from "../ui/card";
-import { MouseEventHandler, useState } from "react";
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { MouseEventHandler } from "react";
+
+import NoballPopver from "./NoballPopver";
 
 function ScoreButtons({
   handleScore,
@@ -10,7 +11,6 @@ function ScoreButtons({
   handleScore: MouseEventHandler<HTMLButtonElement>;
   ballEvents: Record<string, string>;
 }) {
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   return (
     <CardContent className="space-y-4 max-sm:p-0">
       <div className="flex gap-2 justify-center w-full">
@@ -52,49 +52,7 @@ function ScoreButtons({
         >
           Wd
         </Button>
-        <Popover
-          open={isPopoverOpen}
-          modal
-          onOpenChange={() => setIsPopoverOpen((prev) => !prev)}
-        >
-          <PopoverTrigger asChild>
-            <Button
-              size="lg"
-              variant="secondary"
-              className="w-full h-16 text-lg text-muted-foreground"
-              value="-3"
-            >
-              NB
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-60">
-            <div className="w-full grid grid-cols-3 gap-2">
-              {Object.keys(ballEvents)
-                .filter(
-                  (ball) => ball !== "-1" && ball !== "-2" && ball !== "-3"
-                )
-                .map((event, i) => (
-                  <Button
-                    key={i}
-                    className={
-                      event === "6"
-                        ? "bg-amber-400 font-semibold text-amber-950 dark:bg-amber-600 dark:text-amber-50"
-                        : event === "4"
-                        ? "bg-emerald-500 text-emerald-50 dark:bg-emerald-600"
-                        : "bg-secondary text-secondary-foreground"
-                    }
-                    value={`-3${event}`}
-                    onClick={(e) => {
-                      handleScore(e);
-                      setIsPopoverOpen(false);
-                    }}
-                  >
-                    {event}
-                  </Button>
-                ))}
-            </div>
-          </PopoverContent>
-        </Popover>
+        <NoballPopver ballEvents={ballEvents} handleScore={handleScore} />
 
         <Button
           size="lg"
