fdb1b32cb357bfbba0d49ccbba4b2a531c88b2c0
soham2k06 <sohmm.dev@gmail.com>
2024-01-28 12:42:38 +0530

    replaced alert dialog into vanilla dialog

--- components/RestartButton.tsx
+++ components/RestartButton.tsx
@@ -1,15 +1,13 @@
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogPortal,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import { Button } from "@/components/ui/button";
 
 import { MouseEventHandler } from "react";
@@ -21,41 +19,43 @@ function RestartButton({
   handleCloseMenu: MouseEventHandler;
 }) {
   return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <Button size="sm" className="w-full" variant="destructive">
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="destructive" className="w-full">
           Restart
         </Button>
-      </AlertDialogTrigger>
-      <AlertDialogPortal>
-        <AlertDialogContent>
-          <div className="border bg-background gap-4 p-6 rounded-md shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg">
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone.
-                <br />
-                It will permanently delete your scores.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel className="w-full" onClick={handleCloseMenu}>
+      </DialogTrigger>
+      <DialogContent>
+        <div className="gap-4 grid m-2 rounded-md border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg">
+          <DialogHeader>
+            <DialogTitle className="text-2xl font-bold">
+              Are you absolutely sure?
+            </DialogTitle>
+            <DialogDescription>
+              This action cannot be undone.
+              <br />
+              It will permanently delete your scores.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <div className="grid grid-flow-col gap-2">
+              <Button onClick={handleCloseMenu} variant="secondary">
                 Cancel
-              </AlertDialogCancel>
-              <AlertDialogAction
+              </Button>
+              <Button
                 onClick={(e) => {
                   onClick(e);
                   handleCloseMenu(e);
                 }}
-                className="w-full"
+                variant="destructive"
               >
                 Restart
-              </AlertDialogAction>
-            </AlertDialogFooter>
-          </div>
-        </AlertDialogContent>
-      </AlertDialogPortal>
-    </AlertDialog>
+              </Button>
+            </div>
+          </DialogFooter>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
 
