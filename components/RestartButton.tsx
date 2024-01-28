d9cbc91712fb40192d5a26bd77211e065cafb565
soham2k06 <sohmm.dev@gmail.com>
2024-01-28 12:30:13 +0530

    alert dialog transition fix

--- components/RestartButton.tsx
+++ components/RestartButton.tsx
@@ -29,7 +29,7 @@ function RestartButton({
       </AlertDialogTrigger>
       <AlertDialogPortal>
         <AlertDialogContent>
-          <div className="border bg-background gap-4 p-6 m-4 rounded-md">
+          <div className="border bg-background gap-4 p-6 rounded-md shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg">
             <AlertDialogHeader>
               <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
               <AlertDialogDescription>
