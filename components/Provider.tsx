0132f0c9621dbcf8608b6068f0e98391842681cf
soham2k06 <sohmm.dev@gmail.com>
2024-03-10 11:07:59 +0530

    reduced unneccassary api calls + query optimisation

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -6,15 +6,30 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 import { Toaster } from "./ui/sonner";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 interface ProviderProps extends ThemeProviderProps {}
 
 export function Provider({ children, ...props }: ProviderProps) {
   const theme = useTheme();
-  const queryClient = new QueryClient();
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000 * 5,
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+      },
+    },
+  });
+
+  const [queryClientState] = useState(() => queryClient);
+
+  const pathName = usePathname();
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClientState}>
       <NextThemesProvider {...props}>
         {children}
         <Toaster
