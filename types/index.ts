14be1a929dbe0938b4db1d44e17553e8b69522bb
soham2k06 <sohmm.dev@gmail.com>
2024-03-11 08:22:52 +0530

    fixed type

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,6 @@
 import { Dispatch, SetStateAction } from "react";
 
-export type EventType = "0" | "1" | "2" | "4" | "6" | "-1" | "-2" | "-3";
+export type EventType = "0" | "1" | "2" | "3" | "4" | "6" | "-1" | "-2" | "-3";
 
 export type OverlayStateProps = {
   open: boolean;
