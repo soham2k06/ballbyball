748c2678cb825fb2f24317ee3d36d7eeedf9acc4
soham2k06 <sohmm.dev@gmail.com>
2024-03-31 15:05:07 +0530

    removed unnecessary code

--- app/(root)/teams/page.tsx
+++ app/(root)/teams/page.tsx
@@ -1,14 +1,6 @@
-import { auth } from "@clerk/nextjs";
-import { toast } from "sonner";
 import TeamList from "@/components/teams/TeamList";
 
 async function Teams() {
-  const { userId } = auth();
-  if (!userId) {
-    toast.error("User not authenticated");
-    return;
-  }
-
   return <TeamList />;
 }
 
