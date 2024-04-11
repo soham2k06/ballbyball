24dea4dc6f2ec1dfda39bfb1a9389ff4634ea1ec
soham2k06 <sohmm.dev@gmail.com>
2024-04-11 22:33:31 +0530

    added target info with 2 switchable content

---
+++ components/scorer/TargetInfo.tsx
@@ -0,0 +1,69 @@
+import { cn, processTeamName } from "@/lib/utils";
+import { useEffect, useState } from "react";
+
+interface TargetInfoProps {
+  target: number;
+  ballsRemaining: number;
+  runs: number;
+  curTeam: string | undefined;
+}
+
+function TargetInfo({
+  ballsRemaining,
+  target,
+  runs,
+  curTeam,
+}: TargetInfoProps) {
+  const [showTarget, setShowTarget] = useState(false);
+
+  const remainingRuns = target - runs;
+
+  const rrr = Math.round((remainingRuns / ballsRemaining) * 6 * 10) / 10;
+
+  const toggleContent = () => setShowTarget(!showTarget);
+
+  //   useEffect(() => {
+  //     const intervalId = setInterval(toggleContent, 3000);
+
+  //     return () => clearInterval(intervalId);
+  //   }, []);
+
+  useEffect(() => {
+    const TOGGLE_INTERVAL = 15000;
+    const intervalId = setInterval(() => {
+      setShowTarget((prevShowTarget) => !prevShowTarget);
+    }, TOGGLE_INTERVAL);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="relative">
+      <div
+        onClick={toggleContent}
+        className={cn(
+          "w-fit cursor-pointer text-sm font-semibold text-muted-foreground transition-opacity duration-500",
+          {
+            "opacity-0": !showTarget,
+          },
+        )}
+      >
+        {processTeamName(curTeam ?? "")} need {remainingRuns} in{" "}
+        {ballsRemaining} balls to win
+      </div>
+      <div
+        onClick={toggleContent}
+        className={cn(
+          "absolute top-0 w-fit cursor-pointer text-sm font-semibold text-muted-foreground transition-opacity duration-500",
+          {
+            "opacity-0": showTarget,
+          },
+        )}
+      >
+        Target: {target} | RRR: {rrr}
+      </div>
+    </div>
+  );
+}
+
+export default TargetInfo;
