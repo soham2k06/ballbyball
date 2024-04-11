07afa9229a5523ce0e91a734bcbdb9bdd604129b
soham2k06 <sohmm.dev@gmail.com>
2024-04-11 20:18:43 +0530

    fixed skeletons and hydration errors

--- components/player-scores/BatsmanScores.tsx
+++ components/player-scores/BatsmanScores.tsx
@@ -16,67 +16,76 @@ function BatsmanScores({
   events,
   onStrikeBatsman,
 }: BatsmanScoresProps) {
-  if (!playerIds) return "Loading...";
   const player1 = usePlayerById(playerIds[0]);
   const player2 = usePlayerById(playerIds[1]);
 
   return (
     <div className="flex w-full flex-col justify-between gap-1 rounded-md bg-muted p-2 text-lg">
       <div className="flex border-b border-muted-foreground/20 pb-1 text-sm font-bold text-muted-foreground">
-        <th className="mr-4 w-full max-w-28 text-left text-[13px] uppercase">
+        <div className="mr-4 w-full max-w-28 text-left text-[13px] uppercase">
           batting
-        </th>
+        </div>
         <div className="grid w-full grid-cols-[1fr_1fr_1fr_1fr_auto] gap-2 text-xs">
-          <th className="text-center">R</th>
-          <th className="text-center">B</th>
-          <th className="text-center">4s</th>
-          <th className="text-center">6s</th>
-          <th className="min-w-8 text-center">SR</th>
+          <div className="text-center">R</div>
+          <div className="text-center">B</div>
+          <div className="text-center">4s</div>
+          <div className="text-center">6s</div>
+          <div className="min-w-8 text-center">SR</div>
         </div>
       </div>
-      {playerIds.map((id, i) => {
-        const player = i === 0 ? player1.player : player2.player;
-        const isOnStrike = onStrikeBatsman === i;
+      {playerIds.length && player1 && (playerIds.length !== 2 || player2) ? (
+        playerIds.map((id, i) => {
+          const player = i === 0 ? player1.player : player2.player;
+          const isOnStrike = onStrikeBatsman === i;
 
-        const legalEvents = events.filter(
-          (ball) => ball.type !== "-2" && id === ball.batsmanId,
-        );
+          const legalEvents = events.filter(
+            (ball) => ball.type !== "-2" && id === ball.batsmanId,
+          );
 
-        const totalRuns = calcRuns(
-          legalEvents.map(({ type }) => type),
-          true,
-        );
-        const totalBalls = legalEvents.filter((ball) =>
-          getIsInvalidBall(ball.type as EventType),
-        ).length;
+          const totalRuns = calcRuns(
+            legalEvents.map(({ type }) => type),
+            true,
+          );
+          const totalBalls = legalEvents.filter((ball) =>
+            getIsInvalidBall(ball.type as EventType),
+          ).length;
 
-        const strikeRate =
-          Math.round((totalBalls ? (totalRuns / totalBalls) * 100 : 0) * 10) /
-          10;
+          const strikeRate =
+            Math.round((totalBalls ? (totalRuns / totalBalls) * 100 : 0) * 10) /
+            10;
 
-        const { fours, sixes } = getBattingStats(legalEvents);
+          const { fours, sixes } = getBattingStats(legalEvents);
 
-        if (!player)
-          return <Skeleton className="h-[22px] w-full bg-foreground/10" />;
+          if (!player)
+            return (
+              <Skeleton key={id} className="h-[44px] w-full bg-foreground/10" />
+            );
 
-        return (
-          <div key={player.id} className="flex items-center text-sm">
-            <td className="mr-4 w-full max-w-28 truncate text-[13px] font-bold">
-              {player.name}
-              {isOnStrike && " **"}
-            </td>
-            <div className="grid w-full grid-cols-[1fr_1fr_1fr_1fr_auto] gap-2">
-              <td className="text-center text-xs tabular-nums">{totalRuns}</td>
-              <td className="text-center text-xs tabular-nums">{totalBalls}</td>
-              <td className="text-center text-xs tabular-nums">{fours}</td>
-              <td className="text-center text-xs tabular-nums">{sixes}</td>
-              <div className="min-w-8 text-center text-xs tabular-nums">
-                {222.2 || strikeRate}
+          return (
+            <div key={player?.id} className="flex items-center text-sm">
+              <div className="mr-4 w-full max-w-28 truncate text-[13px] font-bold">
+                {player?.name}
+                {isOnStrike && " **"}
+              </div>
+              <div className="grid w-full grid-cols-[1fr_1fr_1fr_1fr_auto] gap-2">
+                <div className="text-center text-xs tabular-nums">
+                  {totalRuns}
+                </div>
+                <div className="text-center text-xs tabular-nums">
+                  {totalBalls}
+                </div>
+                <div className="text-center text-xs tabular-nums">{fours}</div>
+                <div className="text-center text-xs tabular-nums">{sixes}</div>
+                <div className="min-w-8 text-center text-xs tabular-nums">
+                  {strikeRate}
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      ) : (
+        <Skeleton className="h-[44px] w-full bg-foreground/10" />
+      )}
     </div>
   );
 }
--- components/player-scores/BowlerScores.tsx
+++ components/player-scores/BowlerScores.tsx
@@ -20,39 +20,39 @@ function BowlerScores({ playerId, events }: BowlerScoresProps) {
   );
 
   return (
-    <table className="flex w-full flex-col gap-1 rounded-md bg-muted p-2 text-lg">
-      <tr className="flex border-b border-muted-foreground/20 pb-1 text-sm font-bold text-muted-foreground">
-        <th className="mr-4 w-full max-w-28 text-left text-[13px] uppercase">
+    <div className="flex w-full flex-col gap-1 rounded-md bg-muted p-2 text-lg">
+      <div className="flex border-b border-muted-foreground/20 pb-1 text-sm font-bold text-muted-foreground">
+        <div className="mr-4 w-full max-w-28 text-left text-[13px] uppercase">
           bowling
-        </th>
-        <div className="grid w-full grid-cols-[1fr_1fr_1fr_auto] gap-2 text-xs">
-          <th className="text-center">O</th>
-          <th className="text-center">W</th>
-          <th className="text-center">R</th>
-          <th className="min-w-14 text-center">ECON</th>
         </div>
-      </tr>
+        <div className="grid w-full grid-cols-[1fr_1fr_1fr_auto] gap-2 text-xs">
+          <div className="text-center">O</div>
+          <div className="text-center">W</div>
+          <div className="text-center">R</div>
+          <div className="min-w-14 text-center">ECON</div>
+        </div>
+      </div>
 
       {player ? (
-        <tr className="flex items-center text-sm">
-          <td className="mr-4 w-full max-w-28 truncate text-[13px] font-bold">
+        <div className="flex items-center text-sm">
+          <div className="mr-4 w-full max-w-28 truncate text-[13px] font-bold">
             {player.name}
-          </td>
-          <div className="grid w-full grid-cols-[1fr_1fr_1fr_auto] gap-2">
-            <td className="text-center text-xs tabular-nums">
-              {getOverStr(totalBalls)}
-            </td>
-            <td className="text-center text-xs tabular-nums">{wickets}</td>
-            <td className="text-center text-xs tabular-nums">{runs}</td>
-            <td className="min-w-14 text-center text-xs tabular-nums">
-              {runRate}
-            </td>
           </div>
-        </tr>
+          <div className="grid w-full grid-cols-[1fr_1fr_1fr_auto] gap-2">
+            <div className="text-center text-xs tabular-nums">
+              {getOverStr(totalBalls)}
+            </div>
+            <div className="text-center text-xs tabular-nums">{wickets}</div>
+            <div className="text-center text-xs tabular-nums">{runs}</div>
+            <div className="min-w-14 text-center text-xs tabular-nums">
+              {runRate}
+            </div>
+          </div>
+        </div>
       ) : (
         <Skeleton className="h-[22px] w-full bg-foreground/10" />
       )}
-    </table>
+    </div>
   );
 }
 
