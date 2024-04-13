512f2fd3a9d5486912b955b7e42fa9d3ef29d620
soham2k06 <sohmm.dev@gmail.com>
2024-04-13 19:24:13 +0530

    Not showing footer on match page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,15 @@
+"use client";
+
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import DarkModeToggle from "./DarkModeToggle";
+import { usePathname } from "next/navigation";
 
 function Footer() {
+  const pathname = usePathname();
+
+  // Not showing footer on match page
+  if (pathname.includes("match/")) return null;
+
   return (
     <div className="sticky bottom-0 mt-4 w-full border-t bg-card/75 p-2 text-sm text-muted-foreground backdrop-blur-lg">
       <div className="mx-auto flex w-full max-w-7xl items-center justify-between">
