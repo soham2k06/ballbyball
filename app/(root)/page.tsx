261b02a0e29ad1b26e9f9046915567e60f9ae40b
soham2k06 <sohmm.dev@gmail.com>
2024-04-22 21:39:14 +0530

    added a photo glimple

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { Github } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+
+import { Github } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+import GlimpseCarousel from "@/components/GlimpseCarousel";
 
 async function page() {
   return (
@@ -38,27 +42,55 @@ async function page() {
           className="mx-8 rounded-lg max-md:mx-auto max-md:w-11/12"
         />
       </section>
-      {/* <section className="py-8 md:py-12 md:pb-12 lg:py-24 lg:pb-20">
-        <h4 className="mb-8 scroll-m-20 text-4xl font-bold tracking-tight">
-          Demo Images
-        </h4>
-        <div className="flex gap-4 max-md:flex-col">
-          <Image
-            src="/banner-demo-light.png"
-            alt="demo"
-            width={360}
-            height={100}
-            className="rounded-lg max-md:mx-auto max-md:w-11/12"
-          />
-          <Image
-            src="/banner-demo-dark.png"
-            alt="demo"
-            width={360}
-            height={100}
-            className="rounded-lg max-md:mx-auto max-md:w-11/12"
-          />
+      <section className="pt-8 md:pt-8 lg:pt-16">
+        <div className="mb-4 flex max-w-3xl flex-col gap-2 md:mb-8">
+          <h2 className="text-balance text-3xl font-bold leading-tight tracking-tight md:text-4xl lg:leading-[1.1]">
+            Feature Glimpse
+          </h2>
+          <span className="max-w-[568px] text-lg text-muted-foreground sm:text-xl">
+            Explore BallByBall: A Visual Journey Through Cricket Stats
+          </span>
         </div>
-      </section> */}
+
+        <div className="flex w-fit gap-6">
+          <div className="overflow-hidden rounded-lg border">
+            <Image
+              src="/worm-chart.png"
+              alt="worm chart demo"
+              width={320}
+              height={100}
+            />
+            <div className="bg-muted p-1 text-center text-muted-foreground">
+              Worm Chart
+            </div>
+          </div>
+          <div className="overflow-hidden rounded-lg border">
+            <Image
+              src="/runrate-chart.png"
+              alt="runrate chart demo"
+              width={320}
+              height={100}
+            />
+            <div className="bg-muted p-1 text-center text-muted-foreground">
+              Runrate Chart
+            </div>
+          </div>
+          <div className="overflow-hidden rounded-lg border">
+            <Image
+              src="/player-stats.png"
+              alt="player stats demo"
+              width={320}
+              height={100}
+            />
+            <div className="bg-muted p-1 text-center text-muted-foreground">
+              Player Stats
+            </div>
+          </div>
+        </div>
+        <div className="md:hidden">
+          <GlimpseCarousel />
+        </div>
+      </section>
     </>
   );
 }
