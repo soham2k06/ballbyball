cd941ea59d42b095c40cfc06db459440e69d1cfd
soham2k06 <sohmm.dev@gmail.com>
2024-04-29 21:50:27 +0530

    💄 ui: mobile responsive footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,10 +11,15 @@ function Footer() {
 
   return (
     <div className="fixed bottom-0 mt-4 w-full border-t bg-card/75 p-2 text-sm text-muted-foreground backdrop-blur-lg">
-      <div className="mx-auto flex w-full max-w-7xl items-center justify-between">
-        <h2 className="text-xl font-semibold tracking-tight">
-          &copy; BallByBall.
-        </h2>
+      <div className="mx-auto flex w-full max-w-7xl items-center justify-between gap-2 max-md:flex-col">
+        <div className="flex items-center gap-4">
+          <h2 className="text-xl font-semibold tracking-tight">
+            &copy; BallByBall.
+          </h2>
+          <div className="md:hidden">
+            <DarkModeToggle />
+          </div>
+        </div>
         <p className="font-medium">
           Built with ❤️ by{" "}
           <a
@@ -24,7 +29,9 @@ function Footer() {
             Soham Bhikadiya
           </a>
         </p>
-        <DarkModeToggle />
+        <div className="max-md:hidden">
+          <DarkModeToggle />
+        </div>
       </div>
     </div>
   );
