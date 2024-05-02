25d7aee2298522996382c0e7918b1995632e56ab
soham2k06 <sohmm.dev@gmail.com>
2024-05-02 20:24:12 +0530

    🧪: added test cases

--- __tests__/app/page.test.tsx
+++ __tests__/app/page.test.tsx
@@ -1,10 +1,26 @@
 import { render, screen } from "@testing-library/react";
-import HomePage from "@/app/(root)/page";
+import TestPage from "@/app/(root)/test/page";
 
-describe("Page", () => {
-  it("renders a heading", () => {
-    render(<HomePage />);
-    const heading = screen.getByTestId("banner-heading");
-    expect(heading).toBeInTheDocument();
-  });
+test("renders a heading", () => {
+  render(<TestPage />);
+  const heading = screen.getByText(/jest test cases/i);
+  expect(heading).toBeInTheDocument();
+});
+
+test("renders an image", () => {
+  render(<TestPage />);
+  const image = screen.getByTitle(/jest test cases/i);
+  expect(image).toBeInTheDocument();
+});
+
+test("renders a textbox", () => {
+  render(<TestPage />);
+  const textbox = screen.getByRole("textbox");
+  const textboxPlaceholder = screen.getByPlaceholderText("enter your name");
+  expect(textbox).toBeInTheDocument();
+  expect(textboxPlaceholder).toBeInTheDocument();
+  expect(textbox).toHaveAttribute("type", "text");
+  expect(textbox).toHaveAttribute("name", "name");
+  expect(textbox).toHaveAttribute("id", "name");
+  expect(textbox).toHaveAttribute("value", "soham");
 });
