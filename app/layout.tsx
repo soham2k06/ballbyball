48f1446b8490725f25165fc6320da948131039ee
soham2k06 <sohmm.dev@gmail.com>
2024-05-22 22:40:49 +0530

    ⚙️ chore: google adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -139,6 +139,7 @@ export default function RootLayout({
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎾</text></svg>"
           />
           <meta name="google-site-verification" content="VoqGlTAQWR5pCGtJXLewyVpsxWiR1LZr4yRhSg30U-o" />
+          <meta name="google-adsense-account" content="ca-pub-7226440547183724"/>
         </head>
         <body className={cn("antialiased", lato.className)}>
           {children}
