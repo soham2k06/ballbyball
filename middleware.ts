759851356774b15a3b45cccfb5c85cb1204c5c67
soham2k06 <sohmm.dev@gmail.com>
2024-06-01 17:42:31 +0530

    ⚙️ chore: added ignoredRotues

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
+  ignoredRoutes: ["/_next", "/", "/scorer", "/guide"],
   publicRoutes: ["/", "/scorer", "/guide"],
 });
 
