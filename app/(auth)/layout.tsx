6dc81a696dac54a4d2e4af8858e191a19043b5a3
soham2k06 <sohmm.dev@gmail.com>
2024-06-17 22:49:23 +0530

    ⚙️ chore: added clerk loader

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,14 @@
+import { ClerkLoaded, ClerkLoading } from '@clerk/nextjs';
+
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex min-h-screen w-full items-center justify-center bg-black">
-      {children}
+      <ClerkLoading>
+        <div className="text-white">Auth Loading...</div>
+      </ClerkLoading>
+      <ClerkLoaded>
+        {children}
+      </ClerkLoaded>
     </main>
   );
 }
