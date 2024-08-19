2f98c71ea5e0b2248c9c4f6b3f760acf2cc5b1b4
soham2k06 <sohmm.dev@gmail.com>
2024-08-19 14:51:35 +0530

    chore: updated instant scoring metadata

--- app/scorer/page.tsx
+++ app/scorer/page.tsx
@@ -2,9 +2,9 @@ import { Metadata } from "next";
 import ScorerLayout from "@/components/scorer/NormalScorerLayout";
 
 export const metadata: Metadata = {
-  title: "Normal scoring - BallByBall",
+  title: "Instant scoring - BallByBall",
   description:
-    "Experience seamless cricket scoring with BallByBall's Main Scorer Page. Easily input scores, view real-time updates, and access detailed analytics. Keep every match memorable and accurate with our comprehensive scoring tools. Sign in and start scoring for free today!",
+    "Experience seamless cricket scoring with BallByBall's Main Scorer Page. Instantly input scores, view real-time updates, and access detailed analytics. Keep every match memorable and accurate with our comprehensive scoring tools. Sign in and start scoring for free today!",
 };
 
 export default function Home() {
