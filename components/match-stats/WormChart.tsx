2050b8e30a8f4956ef24c7716353904ce9e113b3
soham2k06 <sohmm.dev@gmail.com>
2024-09-03 22:47:44 +0530

    fix: worm chart

--- components/match-stats/WormChart.tsx
+++ components/match-stats/WormChart.tsx
@@ -15,9 +15,11 @@ import { Card, CardContent, CardTitle } from "../ui/card";
 function WormChart({
   ballEvents,
   teams,
+  totalOvers,
 }: {
   ballEvents: EventType[][];
   teams: Team[];
+  totalOvers: number;
 }) {
   const combineData = () => {
     const data = [];
@@ -28,7 +30,11 @@ function WormChart({
     let team1TotalWickets = 0;
     let team2TotalWickets = 0;
 
-    const maxBalls = Math.max(ballEvents[0].length, ballEvents[1].length);
+    const maxBalls = Math.max(
+      ballEvents[0].length,
+      ballEvents[1].length,
+      totalOvers * 6,
+    );
 
     for (let i = 0; i < maxBalls; i++) {
       const curEvent1 = ballEvents[0]?.[i];
@@ -77,22 +83,39 @@ function WormChart({
 
   const CustomDot1 = (props: JSX.Element["props"]) => {
     const { cx, cy, index } = props;
+
+    const r = 3;
     if (
       chartData[index].wickets?.[teams[0].name] >
       chartData[index - 1]?.wickets?.[teams[0].name]
     )
-      return <circle cx={cx} cy={cy} r={4} fill="#1d4ed8" />;
+      return (
+        <circle
+          cx={cx - r / (r * 2)}
+          cy={cy - r / (r * 2)}
+          r={r}
+          fill="#1d4ed8"
+        />
+      );
     else return null;
   };
 
   const CustomDot2 = (props: JSX.Element["props"]) => {
     const { cx, cy, index } = props;
 
+    const r = 3;
     if (
       chartData[index].wickets?.[teams[1].name] >
       chartData[index - 1]?.wickets?.[teams[1].name]
     )
-      return <circle cx={cx} cy={cy} r={4} fill="#b45309" />;
+      return (
+        <circle
+          cx={cx - r / (r * 2)}
+          cy={cy - r / (r * 2)}
+          r={r}
+          fill="#b45309"
+        />
+      );
     else return null;
   };
 
