ea0e7715732241aee2fe003da5bcea58fa728fc0
soham2k06 <sohmm.dev@gmail.com>
2024-09-09 14:13:35 +0530

    fix: win info in player matches

--- app/api/players/matches/[id]/route.ts
+++ app/api/players/matches/[id]/route.ts
@@ -21,6 +21,7 @@ export async function GET(
         allowSinglePlayer: true,
         createdAt: true,
         matchTeams: {
+          orderBy: { batFirst: "desc" },
           select: {
             team: {
               select: {
