c01f71a7edc094937bdb5ba49c796dbb5db8c784
soham2k06 <sohmm.dev@gmail.com>
2024-09-15 19:18:24 +0530

    fix: did not bowl when bowler conceded 0 runs

--- features/players/stats/player-matches.tsx
+++ features/players/stats/player-matches.tsx
@@ -67,7 +67,7 @@ function PlayerMatches({ playerId, setPlayerMatchesOpen }: PlayerMatchesProps) {
               data.map((match) => {
                 const didBat = match.batScore.totalBalls > 0;
                 const didBowl =
-                  match.bowlScore.totalBalls > 0 && match.bowlScore.runs > 0;
+                  match.bowlScore.totalBalls > 0 || match.bowlScore.runs > 0;
                 const isNotout = match.batScore.isNotout;
 
                 return (
